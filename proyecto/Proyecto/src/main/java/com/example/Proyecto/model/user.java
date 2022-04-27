fc2593dd3b2b87ac6532239c3427aca32bf7e367
sebaa2 <48490346+sebaa2@users.noreply.github.com>
2022-04-27 13:14:29 -0400

    Add files via upload

---
+++ proyecto/Proyecto/src/main/java/com/example/Proyecto/model/user.java
@@ -0,0 +1,52 @@
+package com.example.Proyecto.model;
+
+import javax.persistence.*;
+
+@Entity
+public class user {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(name = "password", nullable = false, unique = true, length = 70)
+    private String password;
+
+
+    public user() {
+
+    }
+
+    public user(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
